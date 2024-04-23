5f15b0e1a48ebf412816cd2bb2341ef100abe5ed
s25508 <s25508@pjwstk.edu.pl>
2024-04-23 16:30:33 +0200

    Final revision

--- Program.cs
+++ Program.cs
@@ -2,10 +2,10 @@ using AnimalRestAPI.Animals;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 builder.Services.AddScoped<IAnimalRepository, AnimalRepository>();
 builder.Services.AddScoped<IAnimalService, AnimalService>();
 var app = builder.Build();
-builder.Services.AddControllers();
 
 
 if (app.Environment.IsDevelopment())
--- obj/Debug/net8.0/AnimalRestAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/AnimalRestAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AnimalRestAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e35fbaee6cdfb236613789e2bdbb3ca3bceef660")]
 [assembly: System.Reflection.AssemblyProductAttribute("AnimalRestAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AnimalRestAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
