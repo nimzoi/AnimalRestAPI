cdd64f7c8387e694406caa6c5631cf6dcba25bc3
s25508 <s25508@pjwstk.edu.pl>
2024-04-23 15:34:50 +0200

    Added new AnimalDTO, start of endpoints.

--- Animals/AnimalController.cs
+++ Animals/AnimalController.cs
@@ -1,17 +1,57 @@
-﻿namespace AnimalRestAPI.Animals;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalRestAPI.Animals;
 
 [ApiController]
 [Route("/api/animals")]
+
 public class AnimalController : ControllerBase
 {
-
     [HttpGet("")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public IActionResult GetAllAnimals([FromQuery] string orderBy)
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult GetAllAnimals([FromQuery] string? orderBy)
     {
-        //TODO: validate orderBy
-        var animals = "All Animals"; //TODO: fetch from DB
+        orderBy ??= "name";
+        string[] validOrderParameters = ["name", "description", "category", "area"];
+        if (!validOrderParameters.Contains(orderBy))
+        {
+            return BadRequest("Cannot sort by: " + orderBy);
+        }
+
+        var animals = "All Animals"; //TODO: fetch from DB, order by orderBy ASC
         return Ok(animals);
     }
-}    
+
+    [HttpPost("")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    public IActionResult CreateAnimal([FromBody] CreateAnimalDTO dto)
+    {
+        //TODO: validate dto?
+        //TODO: make input JSON2 (application/*+json ?)
+        var success = true; //TODO: save in DB
+        return success ? StatusCode(StatusCodes.Status201Created) : Conflict();
+    }
+
+    [HttpPut("{idAnimal:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult UpdateAnimal([FromBody] CreateAnimalDTO dto) //#TODO
+    {
+
+        var success = true; 
+        return success ? StatusCode(StatusCodes.Status200OK) : Conflict();
+    }
+
+    [HttpDelete("{idAnimal:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult DeleteAnimal()
+    {
+        var success = true; 
+        return success ? StatusCode(StatusCodes.Status200OK) : Conflict();
+    }
+}
